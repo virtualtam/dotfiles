a57f6895c456bdd032b1ce8c19a9c3092f3a8d61
VirtualTam <virtualtam@flibidi.net>
2016-04-16 17:36:26 +0200

    awesome: enable theme, set wallpaper
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config/awesome/themes/virtualtam/theme.lua
+++ config/awesome/themes/virtualtam/theme.lua
@@ -1,6 +1,6 @@
----------------------------
--- Default awesome theme --
----------------------------
+--------------------------------
+-- VirtualTam's awesome theme --
+--------------------------------
 
 theme = {}
 
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
+theme.wallpaper = "~/.wallpaper.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
@@ -96,4 +96,3 @@ theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 theme.icon_theme = nil
 
 return theme
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
