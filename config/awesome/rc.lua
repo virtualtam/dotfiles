1221ca308905609be95963698fbfb0ee211a8fc7
VirtualTam <virtualtam@flibidi.net>
2016-04-16 17:36:26 +0200

    awesome: enable theme, set wallpaper
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/virtualtam/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
