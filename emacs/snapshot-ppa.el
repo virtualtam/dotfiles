eee9598120d50b0d028a0d9e8b69dd8055a861f1
VirtualTam <virtualtam@flibidi.net>
2016-11-30 23:55:37 +0100

    emacs: update mode configuration
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- emacs/snapshot-ppa.el
+++
@@ -1,7 +0,0 @@
-;;------------------------
-;; emacs-snapshot PPA hack
-;; ppa:ubuntu-elisp/ppa
-;; https://launchpad.net/~ubuntu-elisp/+archive/ubuntu/ppa
-;;------------------------
-(when (>= emacs-major-version 25)
-  (defconst debian-emacs-flavor 'emacs25))
