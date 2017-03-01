bb27f4166aaefcf9a1bb87827109ecfd21c995b1
VirtualTam <virtualtam@flibidi.net>
2017-03-02 00:21:56 +0100

    bash: update pager options
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,6 +1,5 @@
 # ~/.bash_aliases
 
-# Direcotries
 function mcd() {
     # create and switch to a directory
     mkdir -p ${1}
@@ -8,15 +7,24 @@ function mcd() {
 }
 alias md='mkdir'
 
-# Editors
-export MANPAGER='most'
-export PAGER='more -f'
-
-# Force TERM to rxvt so emacs doesn't mess up with Solarized
+# editor
 export EDITOR='emacs'
 export VISUAL='emacs'
 
-# GnuPG
+# less colors
+export LESS_TERMCAP_mb=$'\e[1;31m'    # begin blinking
+export LESS_TERMCAP_md=$'\e[1;31m'    # begin bold
+export LESS_TERMCAP_me=$'\e[0m'       # end mode
+export LESS_TERMCAP_se=$'\e[0m'       # end standout-mode
+export LESS_TERMCAP_so=$'\e[0;40;33m' # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\e[0m'       # end underline
+export LESS_TERMCAP_us=$'\e[1;32m'    # begin underline
+
+# pager
+export MANPAGER='less -s -M +Gg'
+export PAGER='less -s -M +Gg'
+
+# GnuPG CLI agent
 export GNUPG_TTY=$(tty)
 
 # ls
