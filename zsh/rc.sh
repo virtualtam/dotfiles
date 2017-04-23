3db36ac00d0725849995d7e6cd4f29d4b101e799
VirtualTam <virtualtam@flibidi.net>
2017-04-23 12:33:34 +0200

    zsh: disable auto title
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -31,7 +31,7 @@ ZSH_THEME="virtualtam"
 # DISABLE_LS_COLORS="true"
 
 # Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
+DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
 # ENABLE_CORRECTION="true"
