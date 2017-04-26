79a81c803c5db2c945e1233b65752d1cf1ae4930
VirtualTam <virtualtam@flibidi.net>
2017-04-26 22:56:06 +0200

    emacs: add helm-gtags
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- emacs/modes-ide.el
+++ emacs/modes-ide.el
@@ -27,6 +27,26 @@
     (global-set-key (kbd "C-x C-f") 'helm-find-files)
     (global-set-key (kbd "C-x b") 'helm-mini)))
 
+(use-package helm-gtags
+  :ensure t
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'helm-gtags-mode)
+    (setq helm-gtags-ignore-case t
+          helm-gtags-auto-update t
+          helm-gtags-use-input-at-cursor t
+          helm-gtags-pulse-at-cursor t))
+  :config
+  (progn
+    (define-key helm-gtags-mode-map (kbd "C-c gc") 'helm-gtags-create-tags)
+    (define-key helm-gtags-mode-map (kbd "C-c gu") 'helm-gtags-update-tags)
+    (define-key helm-gtags-mode-map (kbd "C-c gs") 'helm-gtags-find-symbol)
+    (define-key helm-gtags-mode-map (kbd "C-c gf") 'helm-gtags-parse-file)
+    (define-key helm-gtags-mode-map (kbd "C-c gg") 'helm-gtags-find-pattern)
+    (define-key helm-gtags-mode-map (kbd "C-c gs") 'helm-gtags-find-symbol)
+    (define-key helm-gtags-mode-map (kbd "C-c gr") 'helm-gtags-find-rtag)
+    (define-key helm-gtags-mode-map (kbd "C-c gt") 'helm-gtags-find-tag)))
+
 (use-package helm-projectile
   :ensure t
   :config
