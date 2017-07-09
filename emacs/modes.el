cef498f5c22f2a356ee4cba7da26cf9e4bb779b2
VirtualTam <virtualtam@flibidi.net>
2017-07-09 14:25:24 +0200

    emacs: update modes
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- emacs/modes.el
+++ emacs/modes.el
@@ -71,14 +71,14 @@
 (use-package sass-mode :ensure t)
 
 ;; Web: html, php, django/jinja
-(use-package php-mode :ensure t)
+(use-package rjsx-mode :ensure t)
 (use-package web-mode
   :ensure t
   :init
   (progn
     (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
-    ;(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
     (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
