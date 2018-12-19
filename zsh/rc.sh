2c29bba06634f00c546404a265c9dba4e51b1737
VirtualTam <virtualtam@flibidi.net>
2018-12-19 01:54:21 +0100

    zsh: start reworking theme
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -12,7 +12,7 @@ fi
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="virtualtam"
+ZSH_THEME="virtualtam2"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
