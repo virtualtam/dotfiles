5a524d94d565fbfb46999a1253281254b13fdba6
VirtualTam <virtualtam@flibidi.net>
2021-01-02 03:09:25 +0100

    iex: tune color and syntax options
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- iex.exs
+++ iex.exs
@@ -1,7 +1,24 @@
-[
-  inspect: [pretty: true],
-  history_size: 20,
-  colors: [],
+IEx.configure([
+  colors: [
+    syntax_colors: [
+      atom: :cyan,
+      boolean: [:green, :bright],
+      nil: :red,
+      number: :green,
+      string: :green,
+    ],
+    ls_directory: :blue,
+    ls_device: :yellow,
+    doc_code: :green,
+    doc_inline_code: :magenta,
+    doc_headings: [:cyan, :underline],
+    doc_title: [:cyan, :bright, :underline],
+  ],
   alive_prompt: "%prefix(%node)%counter>",
-  default_prompt: "%prefix(%counter)>"
-]
+  default_prompt: "%prefix(%counter)>",
+  history_size: 50,
+  inspect: [
+    charlists: :as_lists,
+    pretty: true,
+  ],
+])
