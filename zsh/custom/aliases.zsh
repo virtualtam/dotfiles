3d96561a9ca9da978badb78d48812d15e39a8920
VirtualTam <virtualtam@flibidi.net>
2022-03-30 18:32:02 +0200

    zsh: add aliases to run a ripgrep search and load it in a vim quickfix window
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -24,3 +24,16 @@ alias tssh='TERM=xterm ssh'
 # tree
 alias tl='tree -ph'
 alias ta='tree -pha'
+
+# vim
+
+# vrg runs a search with ripgrep and opens the results in a Vim quickfix window.
+function vrg() {
+    vim -q<(rg --vimgrep --smart-case $@) +"copen 15"
+}
+
+# vvrg runs a search with ripgrep and opens the results in a Vim quickfix window
+# (vertical split).
+function vvrg() {
+    vim -q<(rg --vimgrep --smart-case $@) +copen +"wincmd L"
+}
