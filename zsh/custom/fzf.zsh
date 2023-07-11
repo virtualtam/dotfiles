a1c66b863f45563446ddbaca2f5e89a42079d919
VirtualTam <virtualtam@flibidi.net>
2023-07-11 23:40:14 +0200

    fzf: add theme options
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

---
+++ zsh/custom/fzf.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+ --color=fg:#d8dee9,bg:#2e3440,hl:#a3be8c
+ --color=fg+:#eceff4,bg+:#2e3440,hl+:#a3be8c
+ --color=info:#eceff4,prompt:#bf616a,pointer:#b48ead
+ --color=marker:#d08770,spinner:#d8dee9,header:#ebcb8b'
