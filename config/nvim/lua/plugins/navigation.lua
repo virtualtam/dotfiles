773f4c0050d88a2bbd0416a99ad2a14e1830ef30
VirtualTam <virtualtam@flibidi.net>
2023-10-27 00:34:23 +0200

    neovim: bind C-P to git ls-files
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -2,14 +2,14 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.3",
+        tag = "0.1.4",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require('telescope.builtin')
 
             -- Built-in picker functions
             -- https://github.com/nvim-telescope/telescope.nvim#pickers
-            vim.keymap.set('n', '<C-P>', builtin.find_files, {})
+            vim.keymap.set('n', '<C-P>', builtin.git_files, {})
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
