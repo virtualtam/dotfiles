cb16a30fa29ac53e8f351f690d863cd22aadf262
VirtualTam <virtualtam@flibidi.net>
2024-05-24 10:56:46 +0200

    neovim: rebind C-P to live-grep
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -9,7 +9,7 @@ return {
 
             -- Built-in picker functions
             -- https://github.com/nvim-telescope/telescope.nvim#pickers
-            vim.keymap.set('n', '<C-P>', builtin.git_files, {})
+            vim.keymap.set('n', '<C-P>', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
