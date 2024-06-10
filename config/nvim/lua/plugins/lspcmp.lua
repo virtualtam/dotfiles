f3ebf60b523ffe27d1e6751dca1e891845613bce
VirtualTam <virtualtam@flibidi.net>
2024-06-10 19:13:24 +0200

    neovim: update LuaSnip setup
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config/nvim/lua/plugins/lspcmp.lua
+++ config/nvim/lua/plugins/lspcmp.lua
@@ -1,6 +1,13 @@
 -- Language Server Protocol & Code Completion
 return {
     {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+    },
+    {
         "hrsh7th/nvim-cmp",
         dependencies = {
             -- LSP
@@ -13,7 +20,6 @@ return {
             "hrsh7th/cmp-cmdline",
 
             -- Snippets
-            "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
 
             -- LSP integrations
