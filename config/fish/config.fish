6429a86108f679e91e4a16b69300820168062763
VirtualTam <virtualtam@flibidi.net>
2025-01-08 23:46:36 +0100

    fish: allow defining host-specific configuration
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,32 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Source host-specific configuration
+set fish_local_dir ~/.config/fish_local
+
+if test -d $fish_local_dir
+    set fish_local_variables $fish_local_dir/fish_variables
+    set fish_local_conf_dir $fish_local_dir/conf.d
+    set fish_local_functions_dir $fish_local_dir/functions
+
+    if test -f $fish_local_variables
+        # Expected syntax for host-specific variables:
+        #
+        # set MY_VARIABLE "host-specific-value"
+        # set -gx PATH $PATH /host/specific/path
+        source $fish_local_variables
+    end
+
+    if test -d $fish_local_conf_dir
+        for func in $fish_local_conf_dir/*.fish
+            source $func
+        end
+    end
+
+    if test -d $fish_local_functions_dir
+        for func in $fish_local_functions_dir/*.fish
+            source $func
+        end
+    end
+end
