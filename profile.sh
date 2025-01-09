e76f55e8b942a0a0ae3891cd376b62d6e14cf5f7
VirtualTam <virtualtam@flibidi.net>
2025-01-09 19:48:12 +0100

    profile: remove unused user bin directory from PATH
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- profile.sh
+++ profile.sh
@@ -14,7 +14,6 @@ export EDITOR="nvim"
 export VISUAL="${EDITOR}"
 
 # User binaries
-[ -d "${HOME}/bin" ] && PATH="${HOME}/bin:${PATH}"
 [ -d "${HOME}/.local/bin" ] && PATH="${HOME}/.local/bin:${PATH}"
 
 # Qt theme management
