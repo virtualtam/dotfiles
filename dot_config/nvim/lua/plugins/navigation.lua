4b35dceedc330ca3cfbec52fdf85a0602a80abd6
VirtualTam <virtualtam@flibidi.net>
2025-03-28 22:26:08 +0100

    neovim: update plugins and tree-sitter grammars
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- dot_config/nvim/lua/plugins/navigation.lua
+++ dot_config/nvim/lua/plugins/navigation.lua
@@ -2,7 +2,7 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.7",
+        tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require('telescope.builtin')
