02f0a30e38a043c5e83e68f88a698acb64fe3b13
VirtualTam <virtualtam@flibidi.net>
2026-06-13 21:11:00 +0200

    neovim: setup which-key
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- private_dot_config/nvim/lua/plugins/navigation.lua
+++ private_dot_config/nvim/lua/plugins/navigation.lua
@@ -8,10 +8,31 @@ return {
 
             -- Built-in picker functions
             -- https://github.com/nvim-telescope/telescope.nvim#pickers
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         end,
     },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            delay = 0,
+            preset = "helix",
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    }
 }
